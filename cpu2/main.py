d452af8f44ff1eb6e66b39d560cdf2d98305173b
lmoyalrh <lmoyal@redhat.com>
2024-01-03 15:46:45 +0000

    sf

---
+++ cpu2/main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import numpy as np
+import os
+from time import perf_counter
+
+a = 2 * np.random.rand(5000, 5000) - 1
+num_threads = int(os.environ.get('OMP_NUM_THREADS', -1))
+
+def mul():
+    t = perf_counter()
+    a@a@a@a@a@a@a@a@a@a@a@a@a
+    print(f'{num_threads:3d} threads: {perf_counter() - t:6.2f} sec.')
+
+
+while True:
+    mul()
