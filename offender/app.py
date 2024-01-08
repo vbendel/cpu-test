c264bcfd404d8ca6e823b809c33018b11042a2fa
lmoyalrh <113632797+lmoyalrh@users.noreply.github.com>
2024-01-08 15:27:28 +0200

    Update app.py
    dcs

--- offender/app.py
+++ offender/app.py
@@ -3,7 +3,7 @@ import numpy as np
 import os
 from time import perf_counter
 
-a = 2 * np.random.rand(5000, 5000) - 1
+a = 2 * np.random.rand(50, 50) - 1
 num_threads = int(os.environ.get('OMP_NUM_THREADS', -1))
 
 def mul():
