d34cac5e14e9cbe4d2a996d097c6dc17978c1b74
Vratislav Bendel <vbendel@redhat.com>
2024-01-10 14:01:14 +0100

    offender: rework for more detailed stats
    Signed-off-by: Vratislav Bendel <vbendel@redhat.com>

--- offender/app.py
+++ offender/app.py
@@ -1,16 +1,31 @@
 #!/usr/bin/env python3
-import numpy as np
-import os
+
+from datetime import datetime
 from time import perf_counter
-
-a = 2 * np.random.rand(5000, 5000) - 1
-num_threads = int(os.environ.get('OMP_NUM_THREADS', -1))
-
-def mul():
-    t = perf_counter()
-    a@a@a@a@a@a@a@a@a@a@a@a@a
-    print(f'{num_threads:3d} threads: {perf_counter() - t:6.2f} sec.')
+import numpy as np
 
 
-while True:
-    mul()
+STAT_REPORT = 10
+
+
+def main():
+
+    stats = []
+
+    a = 2 * np.random.rand(5000, 5000) - 1
+
+    while True:
+        for i in range(0, STAT_REPORT):
+            t_0 = perf_counter()
+            a@a
+            t_1 = perf_counter()
+            stats.append(t_1 - t_0)
+        stats = sorted(stats)
+        sum_var = sum(stats)
+        print(f'{datetime.now()}: sum={sum_var:.6f} avg={sum_var/STAT_REPORT:.6f} max={stats[-1]:.6f} min={stats[0]:.6f} median={stats[STAT_REPORT//2]:.6f} p90={stats[-2]:.6f}')
+        print(f'{stats}')
+        stats.clear()
+
+
+if __name__ == "__main__":
+    main()
