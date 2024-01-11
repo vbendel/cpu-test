75f3393c1ea0cdf04bf2de843e4c33df8c32cf78
Vratislav Bendel <vbendel@redhat.com>
2024-01-11 09:52:44 +0100

    offender: more multiplies and global stats
    Signed-off-by: Vratislav Bendel <vbendel@redhat.com>

--- offender/app.py
+++ offender/app.py
@@ -7,24 +7,60 @@ import numpy as np
 
 STAT_REPORT = 10
 
+stats_global = {
+    "sum": 0,
+    "cnt": 0,
+    "max": float("-inf"),
+    "min": float("inf"),
+}
+
+
+def account_run(time: int):
+    global stats_global
+    stats_global["sum"] += time
+    stats_global["cnt"] += 1
+    if time > stats_global["max"]:
+        stats_global["max"] = time
+    if time < stats_global["min"]:
+        stats_global["min"] = time
+
 
 def main():
-
-    stats = []
+    global stats_global
 
     a = 2 * np.random.rand(5000, 5000) - 1
 
     while True:
+        stats_run = []
         for i in range(0, STAT_REPORT):
             t_0 = perf_counter()
-            a@a
+            a @ a @ a @ a @ a @ a @ a @ a @ a @ a @ a @ a @ a
             t_1 = perf_counter()
-            stats.append(t_1 - t_0)
-        stats = sorted(stats)
+            time = t_1 - t_0
+            stats_run.append(time)
+            account_run(time)
+            print(f"Multiply run: {stats_run[-1]:.6f}")
+        stats = sorted(stats_run)
+        stats_run.clear()
         sum_var = sum(stats)
-        print(f'{datetime.now()}: sum={sum_var:.6f} avg={sum_var/STAT_REPORT:.6f} max={stats[-1]:.6f} min={stats[0]:.6f} median={stats[STAT_REPORT//2]:.6f} p90={stats[-2]:.6f}')
-        print(f'{stats}')
-        stats.clear()
+        print(f'{datetime.now()}')
+        print(
+            'Last batch: ' +
+            f'sum={sum_var:.6f} ' +
+            f'avg={sum_var/STAT_REPORT:.6f} ' +
+            f'max={stats[-1]:.6f} ' +
+            f'min={stats[0]:.6f} ' +
+            f'median={stats[STAT_REPORT//2]:.6f} ' +
+            f'p90={stats[-2]:.6f}'
+        )
+        print(
+            'Global stats: ' +
+            f'sum={stats_global["sum"]:.6f} ' +
+            f'cnt={stats_global["cnt"]} ' +
+            f'avg={stats_global["sum"]/stats_global["cnt"]:.6f} ' +
+            f'max={stats_global["max"]} ' +
+            f'min={stats_global["min"]}'
+        )
 
 
 if __name__ == "__main__":
